3c2f913c93282cc4c693cb0e8dd9e06339ac6f5b
aviri <aviri@LAPTOP-C0TB65Q9>
2022-03-02 23:49:45 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class ComplexNumbers
+    {
+        public void GetNumbers(int parameterN)
+        {
+            // Метод напишет последовательность чисел от 1 до параметра N.
+            for (var i = 1; i < parameterN; i++)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.Write(parameterN + ".");
+            Console.WriteLine();
+        }
+        public void GetSquare(int parameterN)
+        {
+            // Если параметр N будет нечетным, метод нарисует квадрат со сторонами(N) и дыркой в центре. 
+            if (parameterN % 2 != 0)
+            {
+                var center = parameterN / 2;
+                for(var j = 0; j < parameterN; j++)
+                {
+                    for (var i = 0; i < parameterN; i++)
+                    {
+                        if (i == center && j == center)
+                        {
+                            Console.Write(" ");
+                        }
+                        else
+                        {
+                            Console.Write("*");
+                        }
+                        
+                    }
+                    Console.Write('\n');
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Квадрат будет построен только при нечетном параметре.");
+            }
+        }
+    }
+}
---
+++ Consoleforpractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ClassLibrary;
+
+namespace Consoleforpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Метод запросит параметр и выполнит 2 функции с ним. Функции из библиотеки классов ClassLibrary.
+            int parameterN = 0;
+            Console.WriteLine("Введите параметр N.");
+            try
+            {
+                parameterN = int.Parse(Console.ReadLine());
+                if (parameterN <= 0)
+                {
+                    Console.WriteLine("Вводимый параметр должен быть положительным.");
+                }
+                else
+                {
+                    ComplexNumbers Numbers = new ComplexNumbers();
+                    Numbers.GetNumbers(parameterN);
+                    Numbers.GetSquare(parameterN);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Вводимый параметр имел неверный формат.");
+            }
+            
+            
+        }
+    }
+}
